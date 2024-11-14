2e67261f7d486ea0ec6ff0dade6b50ccfda72b86
CrazyCat <paveldomkin@gmail.com>
2024-11-14 19:14:36 +0300

    V1.2

--- LabOS2/src/main_copy.c
+++ LabOS2/src/main_copy.c
@@ -5,11 +5,11 @@
 #include <unistd.h>
 #include <stdarg.h>
 #include <time.h>
-#include <stdatomic.h>
 
 #define DECK_NUM 52
 
-atomic_int matches = 0;
+int matches = 0;
+pthread_mutex_t m;
 
 void PrintHelper(const char *fmt, ...) {
 	va_list args;
@@ -23,8 +23,8 @@ void PrintHelper(const char *fmt, ...) {
 }
 
 void *ThreadFunction(void *args) {
-	size_t round = ((size_t *)args)[0];
-	unsigned int seed = ((size_t *)args)[1];
+	size_t round = *(size_t *)args;
+    unsigned int seed = ((size_t *)args)[1];
 	int local = 0;
 	int deck[DECK_NUM];
 	for (int j = 0; j < DECK_NUM; j++) {
@@ -34,6 +34,7 @@ void *ThreadFunction(void *args) {
 	for (size_t i = 0; i < round; i++) {
 		for (int j = DECK_NUM - 1; j > 0; j--) {
 			int k = rand_r(&seed) % (j + 1);
+
 			int temp = deck[j];
 			deck[j] = deck[k];
 			deck[k] = temp;
@@ -43,8 +44,9 @@ void *ThreadFunction(void *args) {
 			++local;
 		}
 	}
-
-	atomic_fetch_add(&matches, local);
+    pthread_mutex_lock(&m);
+	matches += local;
+    pthread_mutex_unlock(&m);
 	return NULL;
 }
 
@@ -58,30 +60,28 @@ int main(int argc, char **argv) {
 	size_t rounds = atol(argv[2]);
 	size_t rounds_for_thread = rounds / threads_num;
 	pthread_t threads[threads_num];
-	size_t thread_args[threads_num][2];
 
-	// Creating threads
+	pthread_mutex_init(&m, NULL);
+
 	for (size_t i = 0; i < threads_num; i++) {
-		thread_args[i][0] = rounds_for_thread;
-		thread_args[i][1] = time(NULL) ^ (i + 1);  // Уникальный seed для каждого потока
-		if (pthread_create(&threads[i], NULL, ThreadFunction, thread_args[i]) != 0) {
+		if (pthread_create(&threads[i], NULL, ThreadFunction, &rounds_for_thread) != 0) {
 			PrintHelper("Error. Thread %zu not created\n", i);
 			exit(EXIT_FAILURE);
 		}
 	}
 
-	// Waiting for finishing threads
 	for (size_t i = 0; i < threads_num; i++) {
 		if (pthread_join(threads[i], NULL) != 0) {
 			PrintHelper("Error. Thread not joined\n");
 			exit(EXIT_FAILURE);
 		}
 	}
+    
+	pthread_mutex_destroy(&m);
 
-	atomic_int count_matches = atomic_load(&matches);
-	PrintHelper("==== %d ====\n", count_matches);
+	PrintHelper("==== %d ====\n", matches);
 
-	double probability = (double)count_matches / (double)rounds;
+	double probability = (double)matches / (double)rounds;
 	PrintHelper("== %.6f ==\n", probability);
 
 	return EXIT_SUCCESS;
